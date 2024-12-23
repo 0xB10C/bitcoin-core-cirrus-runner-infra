8524f1d2319531f577d91cb8592c48f71991aa9c
0xb10c <b10c@b10c.me>
2024-12-23 15:45:01 +0100

    change: decrease nix-store retention
    With the qemu VM isolation, we have about 1.5 GB of store artefacts
    per VM. Old artefacts don't need to be kept around for long.

--- base.nix
+++ base.nix
@@ -38,7 +38,7 @@
     gc = {
       automatic = true;
       dates = "daily";
-      options = "--delete-older-than 30d";
+      options = "--delete-older-than 7d";
     };
     settings = {
       experimental-features = [
